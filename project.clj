e5fe28ee5b39a481a051d8b998c1e8c19fa2af06
Kyle Isom <coder@kyleisom.net>
2012-01-28 20:50:04 +0300

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject dystopia "0.1.0-preview"
+            :description "Web-based front-end for an eBook library."
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [noir "1.2.1"]
+                           [clojureql "1.0.0"]]
+            :main dystopia.server)
+
---
+++ src/dystopia/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns dystopia.views.welcome
+  (:require [dystopia.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to dystopia"]))
