839d4f789f5c39266df7bab3db38b7bb560685d9
Kyle Isom <coder@kyleisom.net>
2012-02-01 07:18:49 +0300

    SQL database driver moved to sqlkorma

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
             :description "Web-based front-end for an eBook library."
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]
-                           [clojureql "1.0.0"]]
+                           [korma "0.2.1"]
+                           [postgresql "9.0-801.jdbc4"]]
             :main dystopia.server)
 
